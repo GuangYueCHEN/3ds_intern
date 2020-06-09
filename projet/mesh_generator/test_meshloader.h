2d9b4fbc13b092378cad1638c436848b23e88c64
CHEN Guangyue (INTERN) <GCN12@dsone.3ds.com>
2020-06-09 11:38:59 +0200

    test_loader

---
+++ projet/mesh_generator/test_meshloader.h
@@ -0,0 +1,27 @@
+#include "catch.h"
+#include "MeshLoader.h"
+#include "Mesh.h"
+#include<vector>
+#include "SimpleShapeGenerator.h"
+#include <iostream>  
+#include <fstream> 
+
+TEST_CASE("test_loader")
+{
+	MeshLoader loader("E:/3ds_intern/projet/mesh_generator/TestData/bunny.obj");
+	Mesh mesh = loader.load();
+	REQUIRE( mesh.nb_triangles()>4000);
+}
+
+TEST_CASE("test_write")
+{
+	SphereGenerator g(4., 6, 6);
+	Mesh mesh = g.run();
+	MeshWriter writer("E:/3ds_intern/projet/mesh_generator/TestResults/sphere.obj");
+	writer.write(mesh);
+	
+	MeshLoader loader("E:/3ds_intern/projet/mesh_generator/TestResults/sphere.obj");
+	Mesh mesh2 = loader.load();
+	REQUIRE(mesh2.is_closed_2());
+}
+
