460993b3b2c3b0b43acdf6e09342640dc9a697b5
CHEN Guangyue (INTERN) <GCN12@dsone.3ds.com>
2020-07-28 09:53:52 +0200

    test

--- trianglecnn/models/layers/mesh_prepare.py
+++ trianglecnn/models/layers/mesh_prepare.py
@@ -186,10 +186,8 @@ def build_gemm(mesh, faces, face_areas, edge_faces):
     mesh.faces_edges = np.array(faces_edges, dtype=np.int32)
     mesh.areas = np.array(face_areas, dtype=np.float32) / np.sum(face_areas)
     '''export_obj(mesh, file="./datasets/test_curvature/%s" % (mesh.filename))'''
-<<<<<<< HEAD
-=======
 
->>>>>>> 3ec3e0166a749ef290ae26d2791ca2c9287c968b
+
 
 def compute_face_normals_and_areas(mesh, faces):
     face_normals = np.cross(mesh.vs[faces[:, 1]] - mesh.vs[faces[:, 0]],
@@ -688,11 +686,7 @@ def curvature_of_vs(mesh):
     for v_id, vertex in enumerate(mesh.vs):
         total_weight_v = total_weight[v_id]
         if total_weight_v == 0.:
-<<<<<<< HEAD
-            continue
-=======
             total_weight_v = 1.
->>>>>>> 3ec3e0166a749ef290ae26d2791ca2c9287c968b
         second_ff_vertices[v_id] = 1./total_weight_v * second_ff_vertices[v_id]
         eigenvalues, eigenvectors = np.linalg.eig(second_ff_vertices[v_id])
         if np.min(eigenvalues) == eigenvalues[0]:
